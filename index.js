59e93c0e16110927cfd0a2d8f4d4eaa2b5a69a24
Programming Hero <programming.hero1@gmail.com>
2022-04-19 23:41:16 -0500

    created post api for keys

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 const app = express();
@@ -45,6 +45,17 @@ async function run(){
         app.get('/productCount', async(req, res) =>{
             const count = await productCollection.estimatedDocumentCount();
             res.send({count});
+        });
+
+        // use post to get products by ids
+        app.post('/productByKeys', async(req, res) =>{
+            const keys = req.body;
+            const ids = keys.map(id => ObjectId(id));
+            const query = {_id: {$in: ids}}
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            console.log(keys);
+            res.send(products);
         })
 
     }
